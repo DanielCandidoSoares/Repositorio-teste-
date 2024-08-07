8604f57949cb1c70617b1066d2b2a031e3ead4bd
DanielCandidoSoares <danigemeos7@gmail.com>
2024-08-07 16:49:54 -0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,31 @@
+data_dia = input("Entre com o dia de seu de nascimento:")
+data_mes = input("Entre com o mês de seu de nascimento:")
+data_ano = input("Entre com o ano de seu de nascimento:")
+print(data_dia,"/",data_mes,"/",data_ano)
+
+if data_mes == "01":
+    print(f"Você nasceu em", data_dia, "de janeiro de", data_ano)
+if data_mes == "02":
+    print(f"Você nasceu em", data_dia, "de fevereiro de", data_ano)
+if data_mes == "03":
+    print(f"Você nasceu em", data_dia, "de março de", data_ano)
+if data_mes == "04":
+    print(f"Você nasceu em", data_dia, "de abril de", data_ano)
+if data_mes == "05":
+    print(f"Você nasceu em", data_dia, "de maio de", data_ano)
+if data_mes == "06":
+    print(f"Você nasceu em", data_dia, "de junho de", data_ano)
+if data_mes == "07":
+    print(f"Você nasceu em", data_dia, "de julho de", data_ano)
+if data_mes == "08":
+    print(f"Você nasceu em", data_dia, "de agosto de", data_ano)
+if data_mes == "09":
+    print(f"Você nasceu em", data_dia, "de setembro de", data_ano)
+if data_mes == "10":
+    print(f"Você nasceu em", data_dia, "de outubro de", data_ano)
+if data_mes == "11":
+    print(f"Você nasceu em", data_dia, "de novembro de", data_ano)
+if data_mes == "12":
+    print(f"Você nasceu em", data_dia, "de dezembro de", data_ano)
+
+
